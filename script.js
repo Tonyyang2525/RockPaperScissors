88bfc240408f9189b5d0a61218108a0d120154d4
Tonyyang2525 <tonyyang2525@gmail.com>
2024-04-28 13:52:43 -0400

    New update with pseudo code

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ function game() {
 
   let userInput = document.forms["rpsForm"]["userInput"].value.toLowerCase();
 
+  // call isValiddation function as a variable
+  // this will return a boolean based off of the users input
   let isValid = isValidation(userInput);
 
+  // if user is not validated then it will not continue by returning
   if (isValid === false) {
     return;
   }
 
+  // this will generate a random number from 0 to 2
+  // the math.floor is a buit in method that rounds the number to the lowest whole number
   const computer = [Math.floor(Math.random() * 3)];
   let result = "";
   let message = "";
@@ -36,10 +41,15 @@ function game() {
   ) {
     message = " you win!";
   }
+
+  //displays the final result withe message of string.
+  // document.getElementById gets the id element within the html file.
+  //innerText means getting the html text
   document.getElementById("displayResult").innerText =
     "Your opponent chose " + result + message;
 }
 
+// this ia a function that validates if the user input is what the program wants. If the input is not then we return a false and it will display an error message.
 function isValidation(userInput) {
   if (
     userInput === "rock" ||
